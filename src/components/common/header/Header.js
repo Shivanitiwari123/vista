7a3e5ccf63b40f40b1f7d78c907f228fcd01f1e0
Shivanitiwari123 <shivani.tiwari@droot.in>
2024-09-05 13:23:39 +0530

    --initial

---
+++ src/components/common/header/Header.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="bg-slate-400">
+      <Image src={""} layout="fill" alt="Logo" />
+    </div>
+  );
+};
+
+export default Header;
