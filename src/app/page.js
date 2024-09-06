aa9eeebc29ef507a70120c4885cd02f84c2eaafe
Shivanitiwari123 <shivani.tiwari@droot.in>
2024-09-06 19:04:58 +0530

    --

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,25 @@
-import Image from "next/image";
+import HomePage from "@/components/HomePage/HomePage";
+import HomePageData from "@/data/homeData.json";
 
 export default function Home() {
+  // Post initial data (POST request)
+  // async function postPageData() {
+  //   const response = await fetch(
+  //     "https://crudcrud.com/api/fec2f164331c4a098b7c0b990c2c2051/home",
+  //     {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify(HomePageData),
+  //     }
+  //   );
+  // }
+  // postPageData();
+
   return (
     <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start min-h-screen bg-gray-50">
-      <h3 className="font-dm_font">Vista Project Center</h3>
+      <HomePage />
     </main>
   );
 }
--- src/components/common/header/Header.js
+++ src/components/common/header/Header.js
@@ -1,10 +1,17 @@
 import Image from "next/image";
 import React from "react";
 
-const Header = () => {
+const Header = ({ data }) => {
   return (
-    <div className="bg-slate-400">
-      <Image src={""} layout="fill" alt="Logo" />
+    <div className="bg-green-400">
+      {data?.menu?.map((item, index) => (
+        <div
+          key={index}
+          className="flex items-center justify-between px-8 py-2 text-white"
+        >
+          <span>{item.text}</span>
+        </div>
+      ))}
     </div>
   );
 };
